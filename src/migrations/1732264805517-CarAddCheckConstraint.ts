658aca471e3ebc4fb428ab0af8ccc22ab59000dd
Nikita_Bezh_(Kross97) <kross97develop@gmail.com>
2024-11-22 13:45:33 +0200

    миграции

---
+++ src/migrations/1732264805517-CarAddCheckConstraint.ts
@@ -0,0 +1,20 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class CarAddCheckConstraint1732264805517 implements MigrationInterface {
+    name = 'CarAddCheckConstraint1732264805517'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "car" DROP COLUMN "model"`);
+        await queryRunner.query(`ALTER TABLE "car" ADD "model" character varying NOT NULL DEFAULT 'vw' CONSTRAINT model_germany_check CHECK ("model" = 'vw' OR "model" = 'bmw' OR "model" = 'mercedes')`);
+        await queryRunner.query(`ALTER TABLE "car" ALTER COLUMN "year" DROP NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "car" ALTER COLUMN "year" SET DEFAULT '2024'`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "car" ALTER COLUMN "year" DROP DEFAULT`);
+        await queryRunner.query(`ALTER TABLE "car" ALTER COLUMN "year" SET NOT NULL`);
+        await queryRunner.query(`ALTER TABLE "car" DROP COLUMN "model"`);
+        await queryRunner.query(`ALTER TABLE "car" ADD "model" integer NOT NULL`);
+    }
+
+}
---
+++ src/migrations/1732270673692-UniqueUserLogin.ts
@@ -0,0 +1,17 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
+
+export class UniqueUserLogin1732270673692 implements MigrationInterface {
+    name = 'UniqueUserLogin1732270673692'
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        // Найти почему рассинхрон со схемой для CHECK констрэйнта
+        // await queryRunner.query(`ALTER TABLE "car" DROP CONSTRAINT "model_germany_check"`);
+        await queryRunner.query(`ALTER TABLE "user" ADD CONSTRAINT "UQ_a62473490b3e4578fd683235c5e" UNIQUE ("login")`);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "user" DROP CONSTRAINT "UQ_a62473490b3e4578fd683235c5e"`);
+        // await queryRunner.query(`ALTER TABLE "car" ADD CONSTRAINT "model_germany_check" CHECK ((((model)::text = 'vw'::text) OR ((model)::text = 'bmw'::text) OR ((model)::text = 'mercedes'::text)))`);
+    }
+
+}
